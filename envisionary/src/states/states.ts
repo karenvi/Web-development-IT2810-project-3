a35a93412dcba0eb3977ecab37740dfde8b3d569
Karen Villmones <karenvi@stud.ntnu.no>
2022-10-13 11:40:35 +0200

    #8 Removed external src-folder

--- envisionary/src/states/states.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from "recoil";
-
-
-// Kan fjerne denne staten, men er greit å ha i begynnelsen så man skjønner hovedprinsippet til recoil!
-export const testRecoilState = atom({
-    key: 'testRecoilID',
-    default: 1,
-  });
