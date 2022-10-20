f3c304e9c718fea834c8ffb65be4c76481ed5031
millieba <millieba@hotmail.com>
2022-10-20 13:46:56 +0200

    #20 Added functioning typedefs + one resolver

--- envisionary/backend/src/index.ts
+++ envisionary/backend/src/index.ts
@@ -21,48 +21,47 @@ async function connect() {
 connect()
 
 // The code below is from https://www.apollographql.com/docs/apollo-server/getting-started/
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
+// A schema is a collection of type definitions (hence "typeDefs") that together define the "shape" 
+// of queries that are executed against your data.
 const typeDefs = `#graphql
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
+  type Country {
+    Rank: String
+    CCA3: String
+    Country: String
+    Capital: String
+    Continent: String
+    Population2022: String
+    Population2020: String
+    Population2015: String
+    Population2010: String
+    Population2000: String
+    Population1990: String
+    Population1980: String
+    Population1970: String
+    Area: String
+    Density: String
+    GrowthRate: String
+    WorldPopulationPercentage: String
   }
 
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
-    books: [Book]
+    countries: [Country]
   }
 `;
 
-const books = [
-    {
-      title: 'The Awakening',
-      author: 'Kate Chopin',
-    },
-    {
-      title: 'City of Glass',
-      author: 'Paul Auster',
-    },
-  ];
-
-// Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array above.
 const resolvers = {
     Query: {
-      books: () => books,
+      countries: () => mongoose.connection.db.collection("countries").find({}).toArray(),
     },
   };
 
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
+// The ApolloServer constructor requires two parameters: your schema definition and your set of resolvers.
 const server = new ApolloServer({
     typeDefs,
     resolvers,
