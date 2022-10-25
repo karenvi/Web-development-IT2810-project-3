a98ece8653592513db0e73a27ffe402befd7fea6
Karen Villmones <karenvi@stud.ntnu.no>
2022-10-25 15:52:14 +0200

    #19 added data to graph

--- envisionary/frontend/src/components/PopulationChart.tsx
+++ envisionary/frontend/src/components/PopulationChart.tsx
@@ -1,71 +1,76 @@
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
-
-const data = [
-    {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  AreaChart,
+  Area
+} from "recharts";
+import { useLocation } from 'react-router-dom';
 
 function PopulationChart() {
+  
+const location = useLocation()
+const data = [
+  {
+    name: "1970",
+    population: location.state.country.Population1970,
+  },
+  {
+    name: "1980",
+    population: location.state.country.Population1980,
+  },
+  {
+    name: "1990",
+    population: location.state.country.Population1990,
+  },
+  {
+    name: "2000",
+    population: location.state.country.Population2000,
+  },
+  {
+    name: "2010",
+    population: location.state.country.Population2010,
+  },
+  {
+    name: "2015",
+    population: location.state.country.Population2015,
+  },
+  {
+    name: "2020",
+    population: location.state.country.Population2020,
+  },
+  {
+    name: "2022",
+    population: location.state.country.Population2022,
+  },
+];
+{/* <p>{}</p> */}
       return (
-        <ResponsiveContainer width="100%" height="100%">
-          <AreaChart
-            width={500}
-            height={400}
-            data={data}
-            margin={{
-              top: 10,
-              right: 30,
-              left: 0,
-              bottom: 0,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
-          </AreaChart>
-        </ResponsiveContainer>
+<ResponsiveContainer height="100%" width="100%">
+        <LineChart
+          width={500}
+          height={100}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+         
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="population" stroke="#8884d8" activeDot={{ r: 8 }} />
+        </LineChart>
+      </ResponsiveContainer>
       );
 }
 
