745f5882a15f31666dcbe8b870935fe0479367ce
Karen Villmones <karenvi@stud.ntnu.no>
2022-10-28 21:12:24 +0200

    #29 Fixed header, removed horizontal annoying scrollbar, and added favicon

--- envisionary/frontend/src/components/Header/Header.tsx
+++ envisionary/frontend/src/components/Header/Header.tsx
@@ -1,13 +1,17 @@
-import { Link } from 'react-router-dom'
-import './Header.css'
+import { Link } from 'react-router-dom';
+import './Header.css';
+import PublicIcon from '@mui/icons-material/Public';
+
 
 function Header() {
 
   return (
     <nav className='header'>
-      <div className='linkContainer'>
-        <Link className='link' to="/">Countries</Link>
+        <div id="logoContainer"><Link id='logoLink' to="/">ENVISIONARY</Link></div>
+        <div className='linkContainer'>
+         <Link className='link' to="/">Countries</Link>
         <Link className='link' to="/give-review">Give review</Link>
+        <Link className='link' to="/info-page">Information</Link>
       </div>
     </nav>
   );
