8c3b9b75936f57db1694c0bea0b42a5e6e97b8d4
millieba <millieba@hotmail.com>
2022-10-28 14:03:35 +0200

    #32 Fixed response in mutation + added explanation

--- envisionary/backend/src/resolvers.ts
+++ envisionary/backend/src/resolvers.ts
@@ -17,16 +17,20 @@ export const resolvers = {
     },
 
     Mutation: {
-        addReview: async (_parent: any, args: IaddReviewArgs) => {
+        addReview: async (_parent: unknown, args: IaddReviewArgs) => {
             const filter = { Country: args.Country };
             const update = { $push: { Reviews: { Name: args.Name, ReviewText: args.ReviewText, Date: args.Date, Rating: args.Rating } } };
-            const options = { new: true, upsert: true };
+            const options = { upsert: true };
 
-            const response = await mongoose.connection.db.collection("countries").findOneAndUpdate(filter, update as unknown, options);
+            // Ideally we would use findOneAndUpdate as it can return the updated document after performing the update operation
+            // However, the type of the update parameter in findOneAndUpdate was not assignable to the update operation we needed 
+            // We therefore decided to use updateOne for the update operation and findOne to fetch the updated document for a response
+            // While making two calls to the database is not ideal, it is still preferred if it allows us to maintain proper type checking + correct response
 
-            console.log(response.value);
+            await mongoose.connection.db.collection("countries").updateOne(filter, update, options);
+            const response = await mongoose.connection.db.collection("countries").findOne(filter);
 
-            return response.value;
+            return response;
         },
     },
 
