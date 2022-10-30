c3bc316365f6770174c0005b1b7fd6a987f78082
Karen Villmones <karenvi@stud.ntnu.no>
2022-10-30 19:35:15 +0100

    38 added favicon and changed header

---
+++ envisionary/frontend/src/pages/InfoPage.tsx
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material";
+
+function InfoPage() {
+
+  return (
+    <>
+    <Typography variant="body1">
+        Information about the site
+    </Typography>
+    </>
+  );
+}
+
+export default InfoPage;
